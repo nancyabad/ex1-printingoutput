3f91929d0abb408ce9cb0593452009e33ee76381
nancyabad <156090892+nancyabad@users.noreply.github.com>
2024-01-23 14:45:50 -0500

    added js file

---
+++ index.js
@@ -0,0 +1,34 @@
+// Task 1
+
+let a = 3;
+let b = 5;
+let c;
+
+let result = `a + b = ${a + b}` + "\n";
+result += `a - b = ${a - b}` + "\n";
+result += `a * b = ${a * b}` + "\n";
+result += `a / b = ${a / b}` + "\n";
+result += `a % b = ${a % b}` + "\n";
+result += `a += b = ${(a += b)}` + "\n";
+result += `a -= b = ${(a -= b)}` + "\n";
+result += `a *= b = ${(a *= b)}` + "\n";
+result += `a /= b = ${(a /= b)}` + "\n";
+result += `a %= b = ${(a %= b)}` + "\n";
+result += `a == b = ${a == b}` + "\n";
+result += `a != b = ${a != b}` + "\n";
+result += `a > b = ${a > b}` + "\n";
+result += `a < b = ${a < b}` + "\n";
+result += `!a && !c = ${!a && !c}` + "\n";
+result += `!a || !c = ${!a || !c}` + "\n";
+
+console.log(result);
+
+// Task 2
+
+let first_name = "Nancy";
+let last_name = "Abad";
+let email = "abad0019@algonquinlive.com";
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+console.log(output);
